5e6d69c715c373c67d6a9dc8f7aa8945c6bbb85d
Kevin Zhan <kevin.w.zhan@gmail.com>
2023-01-17 17:19:17 -0500

    import tarodev player

--- Assets/Scripts/Controller2D.cs
+++ Assets/Scripts/Controller2D.cs
@@ -1,38 +1,78 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 [RequireComponent(typeof(BoxCollider2D))]
 public class Controller2D : MonoBehaviour {
-    
+    public LayerMask collisionMask;
+
     private const float SkinWidth = 0.15f;
     public int xRayCount = 4;
     public int yRayCount = 4;
 
     private float _xRaySpacing;
     private float _yRaySpacing;
-    
+
     private BoxCollider2D _collider;
     private RayCastOrigins _rayCastOrigins;
-    
-    // Start is called before the first frame update
-    void Start() {
-        _collider = GetComponent<BoxCollider2D>();
-    }
-    
-    // Update is called once per frame
-    void Update() {
-        UpdateRaycastOrigins();
-        CalculateRaySpacing();
 
+    // Start is called before the first frame update
+    void Awake() {
+        _collider = GetComponent<BoxCollider2D>();
+        CalculateRaySpacing();
+    }
+
+    public void Move(Vector2 velocity) {
+        UpdateRaycastOrigins();
+
+        if (velocity.x != 0)
+            HorizontalCollisions(ref velocity);
+        if (velocity.y != 0)
+            VerticalCollisions(ref velocity);
+
+        transform.Translate(velocity);
+        Physics2D.SyncTransforms();
+    }
+
+    void HorizontalCollisions(ref Vector2 velocity) {
+        int xDirection = Math.Sign(velocity.x);
+        float rayLength = Mathf.Abs(velocity.x) + SkinWidth;
         for (int i = 0; i < yRayCount; i++) {
-            Debug.DrawRay(_rayCastOrigins.bottomLeft + Vector2.right * (_yRaySpacing * i), Vector2.down, Color.red);
+            Vector2 rayOrigin = xDirection == -1 ? _rayCastOrigins.bottomLeft : _rayCastOrigins.bottomRight;
+            rayOrigin += Vector2.up * (_xRaySpacing * i);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * xDirection, rayLength, collisionMask);
+
+            // Show Rays
+            Debug.DrawRay(rayOrigin, Vector2.right * (xDirection * rayLength), Color.red);
+
+            if (hit) {
+                velocity.x = (hit.distance - SkinWidth) * xDirection;
+                rayLength = hit.distance;
+            }
         }
     }
 
+    void VerticalCollisions(ref Vector2 velocity) {
+        int yDirection = Math.Sign(velocity.y);
+        float rayLength = Mathf.Abs(velocity.y) + SkinWidth;
+        for (int i = 0; i < yRayCount; i++) {
+            Vector2 rayOrigin = yDirection == -1 ? _rayCastOrigins.bottomLeft : _rayCastOrigins.topLeft;
+            rayOrigin += Vector2.right * (_yRaySpacing * i + velocity.x);
+            RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * yDirection, rayLength, collisionMask);
+
+            // Show Rays
+            Debug.DrawRay(rayOrigin, Vector2.up * (yDirection * rayLength), Color.red);
+
+            if (hit) {
+                velocity.y = (hit.distance - SkinWidth) * yDirection;
+                rayLength = hit.distance;
+            }
+        }
+    }
+
+
     void UpdateRaycastOrigins() {
         Bounds bounds = _collider.bounds;
-        bounds.Expand(SkinWidth * -1);
+        bounds.Expand(SkinWidth * -2);
 
         _rayCastOrigins.bottomLeft = new Vector2(bounds.min.x, bounds.min.y);
         _rayCastOrigins.bottomRight = new Vector2(bounds.max.x, bounds.min.y);
@@ -42,8 +82,8 @@ public class Controller2D : MonoBehaviour {
 
     void CalculateRaySpacing() {
         Bounds bounds = _collider.bounds;
-        bounds.Expand(SkinWidth * -1);     
-        
+        bounds.Expand(SkinWidth * -2);
+
         xRayCount = Mathf.Clamp(xRayCount, 2, int.MaxValue);
         yRayCount = Mathf.Clamp(yRayCount, 2, int.MaxValue);
 
@@ -55,6 +95,4 @@ public class Controller2D : MonoBehaviour {
         public Vector2 topLeft, topRight;
         public Vector2 bottomLeft, bottomRight;
     }
-
-
 }
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,21 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Vector2 = System.Numerics.Vector2;
 
 [RequireComponent(typeof(Controller2D))]
 public class PlayerScript : MonoBehaviour {
     // private Rigidbody2D _rb;
-    private float _vX;
-    public float runSpeed = 1;
+    public float gravity = 10;
+    public float moveSpeed = 1;
+    private PlayerControls _playerControls;
+    private Vector2 _velocity;
 
     private Controller2D _controller;
-    
+
     // Start is called before the first frame update
-    void Start() {
-        // _rb = GetComponent<Rigidbody2D>();
+    void Awake() {
         _controller = GetComponent<Controller2D>();
+        _playerControls = new PlayerControls();
+    }
+
+    private void OnEnable() {
+        _playerControls.Player.Enable();
+    }
+
+    private void OnDisable() {
+        _playerControls.Player.Disable();
+    }
+
+    public void OnMove() {
+        
+    }
+
+    private void Update() {
+
+        Vector2 moveInput = _playerControls.Player.Move.ReadValue<Vector2>();
+
+        _velocity.x = moveInput.x * moveSpeed;
+        _velocity.y -= gravity * Time.deltaTime;
+        _controller.Move(_velocity * Time.deltaTime);
     }
 
     // // Update is called once per frame
