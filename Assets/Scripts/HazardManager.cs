fcc0b9a7a5f5c32f04522f5d2d194ee8487e0c18
Kevin Zhan <kevin.w.zhan@gmail.com>
2023-01-24 19:46:43 -0500

    fix zareen's playermovement error

--- Assets/Scripts/HazardManager.cs
+++ Assets/Scripts/HazardManager.cs
@@ -3,12 +3,6 @@ using UnityEngine;
 public class HazardManager : MonoBehaviour {
     // Start is called before the first frame update
     public PlayerStateManager playerStateManager;
-    void Start() {
-    }
-
-    // Update is called once per frame
-    void Update() {
-    }
 
     private void OnCollisionEnter2D(Collision2D col) {
         Debug.Log("Collision detected");
--- Assets/Scripts/Zareen_Code/PlayerMovement.cs
+++ Assets/Scripts/Zareen_Code/PlayerMovement.cs
@@ -3,66 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    protected PlayerAttributes pa;
+public class PlayerMovement : MonoBehaviour {
+    private PlayerAttributes _pa;
 
-    public void Awake()
-    {
-        pa = new PlayerAttributes();
-
+    public void Awake() {
+        _pa = GetComponent<PlayerAttributes>();
         /*Debug.Log("Player movement instance"+pa.pi.playerMovement+" is stored in playerInfo");*/
-
     }
 
-    private void Start()
-    {
-        pa.pi.gravity = -(2 * pa.maxjumpHeight) / Mathf.Pow(pa.timetoApex, 2);
-        pa.pi.maxjumpVelocity = Mathf.Abs(pa.pi.gravity) * pa.timetoApex;
-        pa.pi.minjumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(pa.pi.gravity) * pa.minjumpHeight);
-        
+    private void Start() {
+        _pa.pi.gravity = -(2 * _pa.maxjumpHeight) / Mathf.Pow(_pa.timetoApex, 2);
+        _pa.pi.maxjumpVelocity = Mathf.Abs(_pa.pi.gravity) * _pa.timetoApex;
+        _pa.pi.minjumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(_pa.pi.gravity) * _pa.minjumpHeight);
     }
 
-    private void Update()
-    {
+    private void Update() {
         CalculateVelocity();
-        pa.pi.controller2D.Move(pa.pi.velocity * Time.deltaTime,pa.pi.directionalInput);
+        _pa.pi.controller2D.Move(_pa.pi.velocity * Time.deltaTime, _pa.pi.directionalInput);
 
-        if (pa.pi.controller2D._Collisioninfo.above || pa.pi.controller2D._Collisioninfo.below)
-        {
-            pa.pi.velocity.y = 0;
+        if (_pa.pi.controller2D._Collisioninfo.above || _pa.pi.controller2D._Collisioninfo.below) {
+            _pa.pi.velocity.y = 0;
         }
     }
 
-    private void OnEnable()
-    {
+    private void OnEnable() {
         PlayerManager.OnDirectionalInput += SetDirectionalInput;
         PlayerManager.OnJumpInput += OnJumpInputDown;
     }
 
-    private void SetDirectionalInput(Vector2 input)
-    {
-        pa.pi.directionalInput = input;
+    private void SetDirectionalInput(Vector2 input) {
+        _pa.pi.directionalInput = input;
     }
 
-    private void OnJumpInputDown()
-    {
-        if (pa.pi.controller2D._Collisioninfo.below)
-        {
-            pa.pi.velocity.y = pa.pi.maxjumpVelocity;
+    private void OnJumpInputDown() {
+        if (_pa.pi.controller2D._Collisioninfo.below) {
+            _pa.pi.velocity.y = _pa.pi.maxjumpVelocity;
         }
     }
 
-    private void OnDisable()
-    {
+    private void OnDisable() {
         PlayerManager.OnDirectionalInput -= SetDirectionalInput;
         PlayerManager.OnJumpInput -= OnJumpInputDown;
     }
 
-    private void CalculateVelocity()
-    {
-        float targetvelocityX= pa.pi.directionalInput.x * pa.speed;
-        pa.pi.velocity.x = Mathf.SmoothDamp(pa.pi.velocity.x, targetvelocityX, ref pa.pi.velocitySmoothing, (pa.pi.controller2D._Collisioninfo.below)? pa.accelerationtimeGrounded: pa.accelerationtimeAirborne);
-        pa.pi.velocity.y += pa.pi.gravity * Time.deltaTime ;
+    private void CalculateVelocity() {
+        float targetVelocityX = _pa.pi.directionalInput.x * _pa.speed;
+        float smoothTime = _pa.pi.controller2D._Collisioninfo.below
+            ? _pa.accelerationtimeGrounded
+            : _pa.accelerationtimeAirborne;
+        _pa.pi.velocity.x =
+            Mathf.SmoothDamp(_pa.pi.velocity.x, targetVelocityX, ref _pa.pi.velocitySmoothing, smoothTime);
+        _pa.pi.velocity.y += _pa.pi.gravity * Time.deltaTime;
     }
 }
