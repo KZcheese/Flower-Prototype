5d06044523c5f2751763d646fd1c2658081febfe
Kevin Zhan <kevin.w.zhan@gmail.com>
2023-01-24 20:05:59 -0500

    fix spike detection issue

--- Assets/Scripts/HazardManager.cs
+++ Assets/Scripts/HazardManager.cs
@@ -4,7 +4,7 @@ public class HazardManager : MonoBehaviour {
     // Start is called before the first frame update
     public PlayerStateManager playerStateManager;
 
-    private void OnCollisionEnter2D(Collision2D col) {
+    private void OnTriggerEnter2D(Collider2D col) {
         Debug.Log("Collision detected");
         if (!col.gameObject.CompareTag("Player")) return;
         playerStateManager.HurtPlayer();
